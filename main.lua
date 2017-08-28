815d3de7b868525266d467b83f69074d95a20cb3
Dario Gabriel <dariogabriel113@gmail.com>
2017-08-28 15:47:34 -0300

    Arquivos do projeto

--- main.lua
+++ main.lua
@@ -4,15 +4,14 @@
 --
 -----------------------------------------------------------------------------------------
 
-local tapCount = 0
-
 local background = display.newImageRect( "background.png", 360, 570 )
 background.x = display.contentCenterX
 background.y = display.contentCenterY
 
-local teto = display.newImageRect( "platform.png", 360, 85 )
-teto.x = display.contentCenterX
-teto.y = display.contentHeight1
+local escafandro = display.newImageRect( "escafandro.png", 112, 112 )
+escafandro.x = display.contentCenterX
+escafandro.y = display.contentCenterY
+escafandro.alpha = 0.8
 
 local paredeEsquerda = display.newImageRect( "platform.png", 1, 900 )
 paredeEsquerda.x = display.contentCenterX-160
@@ -22,37 +21,23 @@ local paredeDireita = display.newImageRect( "platform.png", 1, 900 )
 paredeDireita.x = display.contentCenterX+160
 paredeDireita.y = display.contentHeight1
 
-local tapText = display.newText( tapCount, display.contentCenterX, 20, native.systemFont, 40 )
-
 local platform = display.newImageRect( "platform.png", 350, 85 )
 platform.x = display.contentCenterX
 platform.y = display.contentHeight
 
-local balloon = display.newImageRect( "balloon.png", 112, 112 )
-balloon.x = display.contentCenterX
-balloon.y = display.contentCenterY
-balloon.alpha = 0.8
+
 
 local physics = require( "physics" )
 physics.start()
 
-physics.addBody( platform, "static" )
-physics.addBody( balloon, "dynamic", { radius=50, bounce=0.3 } )
-physics.addBody( teto, "static" )
+physics.addBody( escafandro, "dynamic", { radius=50, bounce=0.0 } )
 physics.addBody( paredeEsquerda, "static" )
 physics.addBody( paredeDireita, "static" )
+physics.addBody( platform, "static" )
 
-local function pushBalloon()
-	balloon:applyLinearImpulse( math.random( -0.76, 0.76 ), -0.75, balloon.x, balloon.y )
-	tapCount = tapCount + 1
-	tapText.text = tapCount
-	
-	somarOuSubtrair = math.random( 1, 2 )
-	if somarOuSubtrair == 1 then
-		balloon.rotation = balloon.rotation + math.random( 10, 60 )
-	else
-		balloon.rotation = balloon.rotation - math.random( 10, 60 )
-	end
+
+local function pushMan()
+  escafandro:applyLinearImpulse( math.random( -0.76, 0.76 ), -0.75, escafandro.x, escafandro.y )
 end
 
-balloon:addEventListener( "tap", pushBalloon )
+escafandro:addEventListener( "tap", pushMan )
